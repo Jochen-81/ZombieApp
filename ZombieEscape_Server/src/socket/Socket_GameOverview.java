611a1e421da5b992980f59581e35d316d30e5682
gillesB <baatzgilles@googlemail.com>
2012-09-25 15:11:54 +0200

    omg! pushed to wrong repo. hope nothing is broken

--- ZombieEscape_Server/src/socket/Socket_GameOverview.java
+++
@@ -1,9 +0,0 @@
-package socket;
-
-public class Socket_GameOverview {
-	public int gameID;
-	public String name;
-	public int amountGamers;
-	public double longitude;
-	public double latitude;
-}
--- ZombieEscape_Server/src/socket/Socket_GamerOverview.java
+++
@@ -1,9 +0,0 @@
-package socket;
-
-public class Socket_GamerOverview {
-
-	public String gamername;
-	public boolean isZombie;
-	public double longitude;
-	public double latitude;
-}
