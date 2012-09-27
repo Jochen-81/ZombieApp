2c87e93d3ea6c3717ff70bece5da8fbc0bf96c36
gillesB <baatzgilles@gmail.com>
2012-09-27 14:07:26 +0200

    added selector to options

--- OptionsViewController.h
+++ OptionsViewController.h
@@ -8,8 +8,17 @@
 
 #import <UIKit/UIKit.h>
 
-@interface OptionsViewController : UIViewController
+@interface OptionsViewController : UIViewController{
+    NSTimer *myTimer;
+}
+
+@property (weak, nonatomic) IBOutlet UISegmentedControl *sgNetworkConnection;
 @property (weak, nonatomic) IBOutlet UITextField *edtIPAddress;
+@property (weak, nonatomic) IBOutlet UILabel *txtConnecting;
 - (IBAction)btnReconnectOnClick:(id)sender;
+- (IBAction)sgNetworkConnectionValueChanged:(id)sender;
+
+- (IBAction)startMyTimer:(id)sender;
+- (IBAction)stopMyTimer:(id)sender;
 
 @end
--- OptionsViewController.m
+++ OptionsViewController.m
@@ -15,7 +15,9 @@
 @end
 
 @implementation OptionsViewController
+@synthesize sgNetworkConnection;
 @synthesize edtIPAddress;
+@synthesize txtConnecting;
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -33,11 +35,14 @@
     
     
     self.view.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"background_v3.png"]];
+    
 }
 
 - (void)viewDidUnload
 {
     [self setEdtIPAddress:nil];
+    [self setSgNetworkConnection:nil];
+    [self setTxtConnecting:nil];
     [super viewDidUnload];
     // Release any retained subviews of the main view.
 }
@@ -45,6 +50,7 @@
 - (void) viewWillAppear:(BOOL)animated{
     [super viewWillAppear:animated];
     edtIPAddress.text = [[PlistHandler getPlistHandler] getServerIPAddress];
+    [self setSelectionAccordingToNetworkStatus];
     
 }
 
@@ -58,4 +64,55 @@
     [[NetWorkCom getNetWorkCom] reconnect];
     
 }
+
+- (IBAction)sgNetworkConnectionValueChanged:(id)sender {
+    if(sgNetworkConnection.selectedSegmentIndex == UISegmentedControlNoSegment || onlySwitchSegment){
+        return;
+    } else if(sgNetworkConnection.selectedSegmentIndex == 0){ //"Connected" is pressed
+        sgNetworkConnection.selectedSegmentIndex = UISegmentedControlNoSegment;
+        txtConnecting.hidden = NO;
+        [self.view setNeedsDisplay];	
+        [[NetWorkCom getNetWorkCom] reconnect];
+        [self startMyTimer:self];
+    } else if(sgNetworkConnection.selectedSegmentIndex == 1){ //"Unconnected" is pressed
+        [[NetWorkCom getNetWorkCom] closeConnection];
+        [self setSelectionAccordingToNetworkStatus];
+        [self stopMyTimer:self];
+    }
+}
+
+BOOL onlySwitchSegment = NO;
+-(void) setSelectionAccordingToNetworkStatus: (NSTimer *)theTimer{
+    return [self setSelectionAccordingToNetworkStatus];
+}
+
+
+-(void) setSelectionAccordingToNetworkStatus{
+    onlySwitchSegment = YES;
+    txtConnecting.hidden = YES;
+    if([[NetWorkCom getNetWorkCom] isConnected]){
+        sgNetworkConnection.selectedSegmentIndex = 0;
+    } else {
+        sgNetworkConnection.selectedSegmentIndex = 1;
+    }
+    onlySwitchSegment = NO;
+}
+
+
+- (IBAction)startMyTimer:(id)sender
+{
+    [myTimer invalidate];
+    
+    myTimer = [NSTimer scheduledTimerWithTimeInterval: 5 target:self selector:
+               @selector(setSelectionAccordingToNetworkStatus:) userInfo:nil repeats:YES];
+    
+            
+}
+
+- (IBAction)stopMyTimer:(id)sender
+{
+    [myTimer invalidate];
+    myTimer = nil;
+}
+
 @end
